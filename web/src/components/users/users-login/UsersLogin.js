945b905931760457b7d45a477cbce08a6a884fbd
AlbertGPe <albertgarciapedrosa@gmail.com>
2023-04-24 20:50:55 +0200

    login register css

--- web/src/components/users/users-login/UsersLogin.js
+++ web/src/components/users/users-login/UsersLogin.js
@@ -2,7 +2,8 @@ import React, { useContext, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import usersService from '../../../services/users'
 import { AuthContext } from '../../../contexts/AuthStore'
-import { useLocation, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import logo from '../../../Images/sole-squad-logo-white.png'
 
 function UsersLogin() { 
   const location = useLocation();
@@ -30,39 +31,55 @@ function UsersLogin() {
 
 
   return (
-    <>
-      {location?.state?.user?.confirm === false && <div className='alert alert-info'>Please check your email and active your account before login!</div>}
-      <form onSubmit={handleSubmit(onLoginSubmit)}>
-        {serverError && <div className='alert alert-danger d-none d-lg-block'>{serverError}</div>}
-        {/* USERNAME */}
-        <div className='m-3'>
-          <label htmlFor="username" className='form-label'>Username</label>
-          <input 
-            type="text"
-            className={`form-control ${errors.username ? 'is-invalid' : ''}`}
-            placeholder='Username' {...register('username', {
-              required: 'User name is required'
-            })} />
-            {errors.username && <div className='invalid-feedback'>{errors.username?.message}</div>}
+    <div className="signup__container">
+      <div className="container__child signup__thumbnailLogin">
+        <div className="thumbnail__content text-center">
+          <h1 className="heading--primary"><img src={logo} alt="SoleSquadLogo" />Welcome Back</h1>
         </div>
+        <div className="signup__overlay"></div>
+      </div>
+      <div className="container__child signup__form">
+        {location?.state?.user?.confirm === false && <div className='alert alert-info'>Please check your email and active your account before login!</div>}
+        <form onSubmit={handleSubmit(onLoginSubmit)}>
+          {serverError && <div className='alert alert-danger d-none d-lg-block'>{serverError}</div>}
 
-        {/* PASSWORD */}
-        <div className='m-3'>
-            <label htmlFor="password" className='form-label'>Password</label>
+          {/* USERNAME */}
+          <div className='form-group'>
+            <label htmlFor="username" className='form-label'>Username</label>
             <input 
-              type="password"
-              className={`form-control ${errors.password ? 'is-invalid' : ''}`}
-              placeholder='********' {...register('password', {
-                required: 'User password is required'
+              type="text"
+              className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+              placeholder='Username' {...register('username', {
+                required: 'User name is required'
               })} />
-              {errors.password && <div className='invalid-feedback'>{errors.password?.message}</div>}
-        </div>
+              {errors.username && <div className='invalid-feedback'>{errors.username?.message}</div>}
+          </div>
 
-        <div className='d-grid m-3'>
-          <button type='submit' className='btn btn-primary'>Login</button>
-        </div>
-      </form>
-    </>
+          {/* PASSWORD */}
+          <div className='form-group'>
+              <label htmlFor="password" className='form-label'>Password</label>
+              <input 
+                type="password"
+                className={`form-control ${errors.password ? 'is-invalid' : ''}`}
+                placeholder='********' {...register('password', {
+                  required: 'User password is required'
+                })} />
+                {errors.password && <div className='invalid-feedback'>{errors.password?.message}</div>}
+          </div>
+
+          <div className="m-t-lg">
+            <ul className="list-inline">
+              <li>
+                <input className="btn btn--form" type="submit" value="Register" />
+              </li>
+              <li>
+                <Link to='/register' className='signup__link'>Not registered yet? Come join the elite!</Link>
+              </li>
+            </ul>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
