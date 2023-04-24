bb0fc7d8c83eda8ff9afc3e29825154c52dfd850
AlbertGPe <albertgarciapedrosa@gmail.com>
2023-04-24 20:24:09 +0200

    register css done

--- web/src/Pages/RegisterPage.js
+++ web/src/Pages/RegisterPage.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import UserForm from '../components/users/users-form/UserForm'
-import Register from '../components/register/Register007'
 
 function RegisterPage() {
   return (
     <>
-      <Register />
+      <UserForm />
     </>
   )
 }
--- web/src/components/users/users-form/UserForm.js
+++ web/src/components/users/users-form/UserForm.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
 import usersService from '../../../services/users'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from '../../../Images/sole-squad-logo-white.png'
+import './UserForm.css'
 
 function UserForm() {
   const { register, handleSubmit, setError, formState: { errors } } = useForm({ mode: 'onBlur' });
@@ -24,61 +26,83 @@ function UserForm() {
   }
 
   return (
-    <form onSubmit={handleSubmit(onUserSubmit)}>
-    {serverError && <div className='alert alert-danger d-none d-lg-block'>{serverError}</div>} {/* DELETE WHEN CHANGING SETSERVERERROR() WITH NAVIGATE */}
+    <div className="signup__container">
+      <div className="container__child signup__thumbnail">
+        <div className="thumbnail__content text-center">
+          <h1 className="heading--primary">Welcome to <img src={logo} alt="SoleSquadLogo" /></h1>
+          <h2 className="heading--secondary">Are you ready to join the elite?</h2>
+        </div>
+        <div className="signup__overlay"></div>
+      </div>
+      <div className="container__child signup__form">
+        <form onSubmit={handleSubmit(onUserSubmit)}>
+        {serverError && <div className='alert alert-danger d-none d-lg-block'>{serverError}</div>} {/* DELETE WHEN CHANGING SETSERVERERROR() WITH NAVIGATE */}
 
-    {/* USERNAME */ } 
-    
-    <div className="m-3">
-      <label htmlFor="username" className="form-label">Username</label>
-      <input 
-        type="text" 
-        className={`form-control ${errors.username ? 'is-invalid' : ''}`} 
-        placeholder="Username" {...register('username', {
-          required: 'User name is required, are you trying to be anonymous?', 
-          minLength: { value: 3, message: "Name needs at least 3 chars, are you a rapper or...?"}
-        })} />
-        {errors.username && <div className='invalid-feedback'>{errors.username?.message}</div>}
-    </div>
+          {/* USERNAME */ } 
 
-    {/* EMAIL */ } 
-    
-    <div className="m-3">
-      <label htmlFor="email" className="form-label">Email address</label>
-      <input 
-        type="email" 
-        className={`form-control ${errors.email ? 'is-invalid' : ''}`} 
-        placeholder='SoleSquad@solesquad.com' {...register('email', {
-          required: 'User email is required...I mean...everyone has an email nowadays',
-          pattern: {
-            value: /^\S+@\S+\.\S+$/,
-            message: 'User email must be valid, look for the tinny details'
-          }
-        })}/>
-        {errors.email && <div className='invalid-feedback'>{errors.email?.message}</div>}
-    </div>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input 
+              type="text" 
+              className={`form-control ${errors.username ? 'is-invalid' : ''}`}
+              id="username" 
+              placeholder="Username" {...register('username', {
+                required: 'User name is required, are you trying to be anonymous?', 
+                minLength: { value: 3, message: "Name needs at least 3 chars, are you a rapper or...?"}
+              })} />
+              {errors.username && <div className='invalid-feedback'>{errors.username?.message}</div>}
+          </div>
 
-    {/* PASSWORD */ } 
-    
-    <div className="m-3">
-      <label htmlFor="password" className="form-label">Password</label>
-      <input 
-        type="password" 
-        className={`form-control ${errors.password ? 'is-invalid' : ''}`} 
-        placeholder='********' {...register('password', {
-          required: 'User password is required unless you want it to be public, hope not',
-          minLength: {
-            value: 8,
-            message: "User password needs at least 8 chars... just try not to be so obvious"
-          }
-        })}/>
-        {errors.password && <div className='invalid-feedback'>{errors.password?.message}</div>}
+          {/* EMAIL */ } 
+
+
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input 
+              type="email" 
+              id="email"
+              className={`form-control ${errors.email ? 'is-invalid' : ''}`} 
+              placeholder='SoleSquad@solesquad.com' {...register('email', {
+                required: 'User email is required...I mean...everyone has an email nowadays',
+                pattern: {
+                  value: /^\S+@\S+\.\S+$/,
+                  message: 'User email must be valid, look for the tinny details'
+                }
+              })}/>
+              {errors.email && <div className='invalid-feedback'>{errors.email?.message}</div>}
+          </div>
+
+          {/* PASSWORD */ } 
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input 
+              type="password"
+              id="password"
+              name="password"
+              className={`form-control ${errors.password ? 'is-invalid' : ''}`} 
+              placeholder='********' {...register('password', {
+                required: 'User password is required unless you want it to be public, hope not',
+                minLength: {
+                  value: 8,
+                  message: "User password needs at least 8 chars... just try not to be so obvious"
+                }
+              })}/>
+              {errors.password && <div className='invalid-feedback'>{errors.password?.message}</div>}
+          </div>
+          <div className="m-t-lg">
+            <ul className="list-inline">
+              <li>
+                <input className="btn btn--form" type="submit" value="Register" />
+              </li>
+              <li>
+                <Link to='/login' className='signup__link'>I am already a member</Link>
+              </li>
+            </ul>
+          </div>
+        </form>  
+      </div>
     </div>
-    
-    <div className='d-grid m-3'>
-      <button type='submit' className='btn btn-primary'>Register</button>
-    </div>
-    </form>
   )
 }
 
